f77c71631a9679a606831a1aeebbea398b8a4e4d
Camila Caxambu <camila-caxambu@hotmail.com>
2019-11-22 01:16:55 -0300

    NewsDoSufoco/

---
+++ NewsDoSufoco/NewsDoSufoco/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NewsDoSufoco.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ NewsDoSufoco/NewsDoSufoco/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewsDoSufoco.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
